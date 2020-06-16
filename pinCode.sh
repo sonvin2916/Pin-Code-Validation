dd330216f7441a3f8ab4679b769c17fd877eb44b
sonvin2916 <sonalchougule07@gmail.com>
2020-06-16 14:29:41 +0530

    UC-4 : Added

--- pinCode.sh
+++ pinCode.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 shopt -s extglob
-echo "Enter the pin code "
-read pin
-pattern="^([0-9]{6})$"
+pin='400 088'
+pattern="^([0-9]){3}[[:space:]][0-9]{3}$"
 if [[ $pin =~ $pattern ]]
 then
-	echo "Valid Pin Code"
+        echo "Valid Pin Code"
 else
-	echo "Invalid Pin code"
+        echo "Invalid Pin code"
 fi
